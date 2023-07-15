8d27d740bc7027dfb0f37c8843cc1a9ce314f948
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2023-07-15 18:08:54 +0545

    initial part 2

--- index.ts
+++ index.ts
@@ -1,10 +1,1 @@
-import express, { Request, Response, NextFunction } from "npm:express";
-import mongoose from "npm:mongoose";
-const app = express();
-const PORT = Deno.env.get("PORT") || 8080;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to the Dinosaur API!");
-});
-
-app.listen(PORT);
+console.log("A");
