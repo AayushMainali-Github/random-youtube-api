f5e395ba05c74265780a497f02111d1f45758345
New 6969 <94561475+AayushMainali-Github@users.noreply.github.com>
2023-07-15 22:15:08 +0545

    link model

---
+++ model/Link.ts
@@ -0,0 +1,8 @@
+import { Schema, model } from "mongoose";
+import { LinkData } from "../utils/interfaces/Link";
+
+const linkSchema = new Schema<LinkData>({
+  link: { name: String, required: true },
+});
+
+const Link = model<LinkData>("Link", linkSchema);
---
+++ utils/interfaces/Link.ts
@@ -0,0 +1,3 @@
+export interface LinkData {
+  link: string;
+}
